867b9d0845a74f4b2508edef4221fe146a586957
mansur61 <kayamansur61@gmail.com>
2026-06-19 15:45:39 +0300

    panle revizeler

--- KtuDeYasPortal.Panel/obj/Debug/net10.0/KtuDeYasPortal.Panel.AssemblyInfo.cs
+++ KtuDeYasPortal.Panel/obj/Debug/net10.0/KtuDeYasPortal.Panel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KtuDeYasPortal.Panel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc44f9f95dcfa8d65e205cccaf406f94612cc1e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("KtuDeYasPortal.Panel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KtuDeYasPortal.Panel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
